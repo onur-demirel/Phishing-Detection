5e107823fd82328817d43e0462546440372feb2b
izgi-a-hub <iremzehraaltun@gmail.com>
2023-12-31 00:46:15 +0300

    xlm-roberta embeddings and xgboost model are created

--- flask/model_build.py
+++ flask/model_build.py
@@ -2,3 +2,40 @@
 # 1. XGBoost
 # 2. CATBoost
 # 3. ANN 3 layer
+
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+import pickle
+from xgboost import XGBClassifier
+
+# Load the embeddings
+with open('embeddings/legitimate_embeddings-xlm-roberta.pkl', 'rb') as f:
+    embeddings = pickle.load(f)
+
+def prepare_data(embeddings):
+    # shuffle the embeddings
+    np.random.shuffle(embeddings)
+
+    labels = embeddings[:, -1]
+    embeddings = embeddings[:, :-1]
+
+    X_train, X_test, y_train, y_test = train_test_split(embeddings, labels, test_size=0.2, random_state=42)
+    return X_train, X_test, y_train, y_test
+
+def xgboost_model(X_train, X_test, y_train, y_test):
+    model = XGBClassifier()
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    print("XGBoost Accuracy: ", (y_pred == y_test).mean())
+
+def create_model():
+    X_train, X_test, y_train, y_test = prepare_data(embeddings)
+    xgboost_model(X_train, X_test, y_train, y_test)
+
+
+if __name__ == '__main__':
+    create_model()
+
+
+
--- flask/prepare_embedding.py
+++ flask/prepare_embedding.py
@@ -4,12 +4,11 @@
 # Run extracted and translated text through Electra to get the embeddings
 
 
-import trafilatura as tr
+# import trafilatura as tr
 from transformers import XLMRobertaModel, XLMRobertaTokenizer, ElectraModel, ElectraTokenizer, AutoTokenizer, AutoModel
 import torch
 import numpy as np
 import os
-import pandas as pd
 import re
 import string
 import time
@@ -18,63 +17,63 @@ import json
 import sys
 
 
-def extract_text():
-    """
-    Extract the text from the html files in the benign and phishing folders
-    :return: None
-    """
-    # Get the html files from the benign and phishing folders
-    benign_mislead_path = sys.argv[1]
-    benign_mislead_files = os.listdir(benign_mislead_path)
-    phishing_path = sys.argv[2]
-    phishing_files = os.listdir(phishing_path)
-    os.makedirs('extracted_benign_misleading_text', exist_ok=True)
-    os.makedirs('extracted_phishing_text', exist_ok=True)
-
-    print("*****benign_mislead_files*****", len(benign_mislead_files))
-
-    # Get the text from the benign and misleading html files
-    for file in benign_mislead_files:
-        try:
-            with open(benign_mislead_path + "/" + file, 'r', encoding="utf-8") as f:
-                html = f.read()
-                text = tr.extract(html)
-                if text is None:
-                    continue
-                with open('extracted_benign_misleading_text/' + file[:-4] + '.txt', 'w+', encoding="utf-8") as f:
-                    print("utf-8 ---", file[:-4])
-                    f.write(text)
-        except UnicodeDecodeError:
-            with open(benign_mislead_path + "/" + file, 'r', encoding="windows-1256") as f:
-                html = f.read()
-                text = tr.extract(html)
-                if text is None:
-                    continue
-                with open('extracted_benign_misleading_text/' + file[:-4] + '.txt', 'w+', encoding="utf-8") as f:
-                    print("windows-1256 ---", file[:-4])
-                    f.write(text)
-
-    print("*****phishing_files*****", len(phishing_files))
-    # Get the text from the phishing html files
-    for file in phishing_files:
-        try:
-            with open(phishing_path + "/" + file, 'r', encoding="utf-8") as f:
-                html = f.read()
-                text = tr.extract(html)
-                if text is None:
-                    continue
-                with open('extracted_phishing_text/' + file[:-4] + '.txt', 'w+', encoding="utf-8") as f:
-                    print("utf-8 ---", file[:-4])
-                    f.write(text)
-        except UnicodeDecodeError:
-            with open(phishing_path + "/" + file, 'r', encoding="windows-1256") as f:
-                html = f.read()
-                text = tr.extract(html)
-                if text is None:
-                    continue
-                with open('extracted_phishing_text/' + file[:-4] + '.txt', 'w+', encoding="windows-1256") as f:
-                    print("windows-1256 ---", file[:-4])
-                    f.write(text)
+# def extract_text():
+#     """
+#     Extract the text from the html files in the benign and phishing folders
+#     :return: None
+#     """
+#     # Get the html files from the benign and phishing folders
+#     benign_mislead_path = sys.argv[1]
+#     benign_mislead_files = os.listdir(benign_mislead_path)
+#     phishing_path = sys.argv[2]
+#     phishing_files = os.listdir(phishing_path)
+#     os.makedirs('extracted_benign_misleading_text', exist_ok=True)
+#     os.makedirs('extracted_phishing_text', exist_ok=True)
+#
+#     print("*****benign_mislead_files*****", len(benign_mislead_files))
+#
+#     # Get the text from the benign and misleading html files
+#     for file in benign_mislead_files:
+#         try:
+#             with open(benign_mislead_path + "/" + file, 'r', encoding="utf-8") as f:
+#                 html = f.read()
+#                 text = tr.extract(html)
+#                 if text is None:
+#                     continue
+#                 with open('extracted_benign_misleading_text/' + file[:-4] + '.txt', 'w+', encoding="utf-8") as f:
+#                     print("utf-8 ---", file[:-4])
+#                     f.write(text)
+#         except UnicodeDecodeError:
+#             with open(benign_mislead_path + "/" + file, 'r', encoding="windows-1256") as f:
+#                 html = f.read()
+#                 text = tr.extract(html)
+#                 if text is None:
+#                     continue
+#                 with open('extracted_benign_misleading_text/' + file[:-4] + '.txt', 'w+', encoding="utf-8") as f:
+#                     print("windows-1256 ---", file[:-4])
+#                     f.write(text)
+#
+#     print("*****phishing_files*****", len(phishing_files))
+#     # Get the text from the phishing html files
+#     for file in phishing_files:
+#         try:
+#             with open(phishing_path + "/" + file, 'r', encoding="utf-8") as f:
+#                 html = f.read()
+#                 text = tr.extract(html)
+#                 if text is None:
+#                     continue
+#                 with open('extracted_phishing_text/' + file[:-4] + '.txt', 'w+', encoding="utf-8") as f:
+#                     print("utf-8 ---", file[:-4])
+#                     f.write(text)
+#         except UnicodeDecodeError:
+#             with open(phishing_path + "/" + file, 'r', encoding="windows-1256") as f:
+#                 html = f.read()
+#                 text = tr.extract(html)
+#                 if text is None:
+#                     continue
+#                 with open('extracted_phishing_text/' + file[:-4] + '.txt', 'w+', encoding="windows-1256") as f:
+#                     print("windows-1256 ---", file[:-4])
+#                     f.write(text)
 
 def create_embeddings():
     """
@@ -83,54 +82,66 @@ def create_embeddings():
     """
     create_xlm_roberta_embeddings()
 
+
+def generate_xlm_roberta_embeddings_arrays(files, path):
+    model = XLMRobertaModel.from_pretrained('xlm-roberta-base')
+    tokenizer = XLMRobertaTokenizer.from_pretrained('xlm-roberta-base')
+    model.eval()
+
+    xlm_roberta_embeddings = np.array([]).reshape(0, 768)
+    xlm_roberta_embeddings_labels = np.array([]).reshape(0, 1)
+
+    print("-" * 100)
+    for file in files:
+        with open(path + "/" + file, 'r', encoding="utf-8") as f:
+            text = f.read()
+        #print(path + "/" + file)
+        encoded_input = tokenizer(text, padding=True, truncation=True, return_tensors='pt')
+        with torch.no_grad():
+            model_output = model(**encoded_input)
+            sentence_embeddings = mean_pooling(model_output, encoded_input['attention_mask'])
+            xlm_roberta_embeddings = np.concatenate((xlm_roberta_embeddings, sentence_embeddings.cpu().numpy()))
+            if path == sys.argv[1]:
+                xlm_roberta_embeddings_labels = np.concatenate(
+                    (xlm_roberta_embeddings_labels, np.zeros((len(sentence_embeddings), 1))))
+
+            else:
+                xlm_roberta_embeddings_labels = np.concatenate(
+                    (xlm_roberta_embeddings_labels, np.ones((len(sentence_embeddings), 1))))
+
+
+    result = np.concatenate((xlm_roberta_embeddings, xlm_roberta_embeddings_labels), axis=1)
+    return result, xlm_roberta_embeddings_labels
 def create_xlm_roberta_embeddings():
     """
     Get the XLM-Roberta embeddings for the extracted text
     :return: None
     """
     # Get the extracted text from the benign and phishing folders
-    benign_mislead_path = sys.argv[3]
+    benign_mislead_path = sys.argv[1]
     benign_mislead_files = os.listdir(benign_mislead_path)
-    phishing_path = sys.argv[4]
+    phishing_path = sys.argv[2]
     phishing_files = os.listdir(phishing_path)
-
-    # Load the XLM-Roberta model and tokenizer
-    model = XLMRobertaModel.from_pretrained('xlm-roberta-base')
-    tokenizer = XLMRobertaTokenizer.from_pretrained('xlm-roberta-base')
-    model.eval()
-    print("*****model*****", model)
-
-    xlm_roberta_embeddings = np.array([]).reshape(0, 768)
-    xlm_roberta_embeddings_labels = np.array([]).reshape(0, 1)
-    with open(benign_mislead_path + "/" + benign_mislead_files[2], 'r', encoding="utf-8") as f:
-        text = f.read()
-        print(benign_mislead_path + "/" + benign_mislead_files[2])
-    encoded_input = tokenizer(text, padding=True, truncation=True, return_tensors='pt')
-    print(len(encoded_input['input_ids'][0]))
-    with torch.no_grad():
-        model_output = model(**encoded_input)
-        print(len(model_output[0]))
-        sentence_embeddings = mean_pooling(model_output, encoded_input['attention_mask'])
-        # xlm_roberta_embeddings = np.concatenate((xlm_roberta_embeddings, sentence_embeddings.cpu().numpy()))
-        # xlm_roberta_embeddings_labels = np.concatenate((xlm_roberta_embeddings_labels, np.zeros((len(sentence_embeddings), 1))))
+    os.makedirs('embeddings', exist_ok=True)
 
 
-    # for file in benign_mislead_files:
-    #     with open(benign_mislead_path + "/" + file, 'r', encoding="utf-8") as f:
-    #         text = f.read()
-    #     encoded_input = tokenizer(text, padding=True, truncation=True, return_tensors='pt')
-    #     with torch.no_grad():
-    #         model_output = model(**encoded_input)
-    #         sentence_embeddings = mean_pooling(model_output, encoded_input['attention_mask'])
-    #         # xlm_roberta_embeddings = np.concatenate((xlm_roberta_embeddings, sentence_embeddings.cpu().numpy()))
-    #         # xlm_roberta_embeddings_labels = np.concatenate((xlm_roberta_embeddings_labels, np.zeros((len(sentence_embeddings), 1))))
-    # print("*****xlm_roberta_embeddings*****", xlm_roberta_embeddings.shape)
-    # print("*****xlm_roberta_embeddings_labels*****", xlm_roberta_embeddings_labels.shape)
+    result_legitimate, label_leg = generate_xlm_roberta_embeddings_arrays(
+        benign_mislead_files, benign_mislead_path)
+    print("result_legitimate shape", result_legitimate.shape)
+    # print("label_leg shape", label_leg.shape)
+    # print("label_leg", label_leg)
+    result_phishing, label_phis = generate_xlm_roberta_embeddings_arrays(
+        phishing_files, phishing_path)
+    print("result_phishing shape", result_phishing.shape)
+    # print("label_phis shape", label_phis.shape)
+    # print("label_phis", label_phis)
+    result = np.concatenate((result_legitimate, result_phishing), axis=0)
 
-    print("Sentence embeddings:")
-    print(sentence_embeddings)
-    print("Sentence embeddings shape:")
-    print(sentence_embeddings.shape)
+    print("result shape", result.shape)
+    print("result", result[:-1, :])
+    with open('embeddings/' + 'legitimate_embeddings-xlm-roberta.pkl', 'wb') as file:
+        pickle.dump(result, file)
+
 
 def mean_pooling(model_output, attention_mask):
     token_embeddings = model_output[0] #First element of model_output contains all token embeddings
