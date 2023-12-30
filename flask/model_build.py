a2c2ab9d5eab052d032ae8c82cdf47b2b9ee2ab2
izgi-a-hub <iremzehraaltun@gmail.com>
2023-12-31 01:18:21 +0300

    update model_build.py
    save trained model

--- flask/model_build.py
+++ flask/model_build.py
@@ -2,6 +2,7 @@
 # 1. XGBoost
 # 2. CATBoost
 # 3. ANN 3 layer
+import os
 
 import pandas as pd
 import numpy as np
@@ -9,33 +10,50 @@ from sklearn.model_selection import train_test_split
 import pickle
 from xgboost import XGBClassifier
 
-# Load the embeddings
-with open('embeddings/legitimate_embeddings-xlm-roberta.pkl', 'rb') as f:
-    embeddings = pickle.load(f)
+class ModelBuilder:
+    def __init__(self):
+        self.X_train, self.X_test, self.y_train, self.y_test = self.load_data()
 
-def prepare_data(embeddings):
-    # shuffle the embeddings
-    np.random.shuffle(embeddings)
+    def load_data(self):
+        # Load the embeddings
+        with open('embeddings/legitimate_embeddings-xlm-roberta.pkl', 'rb') as f:
+            embeddings = pickle.load(f)
 
-    labels = embeddings[:, -1]
-    embeddings = embeddings[:, :-1]
+        # shuffle the embeddings
+        np.random.shuffle(embeddings)
 
-    X_train, X_test, y_train, y_test = train_test_split(embeddings, labels, test_size=0.2, random_state=42)
-    return X_train, X_test, y_train, y_test
+        labels = embeddings[:, -1]
+        embeddings = embeddings[:, :-1]
 
-def xgboost_model(X_train, X_test, y_train, y_test):
-    model = XGBClassifier()
-    model.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
-    print("XGBoost Accuracy: ", (y_pred == y_test).mean())
+        X_train, X_test, y_train, y_test = train_test_split(embeddings, labels, test_size=0.2, random_state=42)
+        return X_train, X_test, y_train, y_test
 
-def create_model():
-    X_train, X_test, y_train, y_test = prepare_data(embeddings)
-    xgboost_model(X_train, X_test, y_train, y_test)
+    def xgboost_model(self, X_train, y_train):
+        model = XGBClassifier()
+        model.fit(X_train, y_train)
+        pickle.dump(model, open("model/xgboost_model.pkl", "wb"))
+
+    def load_model(self):
+        model = pickle.load(open("model/xgboost_model.pkl", "rb"))
+        return model
+
+    def test_model(self, model, X_test, y_test):
+        y_pred = model.predict(X_test)
+        print(y_pred)
+        print(y_test)
+        print("Accuracy:", np.mean(y_pred == y_test))
+
+    def create_model(self, X_train, y_train):
+        self.xgboost_model(X_train, y_train)
 
 
 if __name__ == '__main__':
-    create_model()
+    os.makedirs('model', exist_ok=True)
+    model_builder = ModelBuilder()
+    X_train, X_test, y_train, y_test = model_builder.load_data()
+    model_builder.create_model(X_train, y_train)
+    model = model_builder.load_model()
+    model_builder.test_model(model, X_test, y_test)
 
 
 
