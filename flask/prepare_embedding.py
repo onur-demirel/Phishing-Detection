9fffd636aed735dc411cd233485f72151367c20c
onur-demirel <85741673+onur-demirel@users.noreply.github.com>
2024-01-01 17:06:49 +0300

    Update prepare_embedding.py

--- flask/prepare_embedding.py
+++ flask/prepare_embedding.py
@@ -6,7 +6,7 @@
 
 # import trafilatura as tr
 from sentence_transformers import SentenceTransformer
-from transformers import XLMRobertaModel, XLMRobertaTokenizer,  AutoTokenizer, AutoModel, ElectraModel, ElectraTokenizer
+from transformers import XLMRobertaModel, XLMRobertaTokenizer, AutoTokenizer, AutoModel, ElectraModel, ElectraTokenizer
 import torch
 import numpy as np
 import os
@@ -77,15 +77,8 @@ from googletrans import Translator
 #                     print("windows-1256 ---", file[:-4])
 #                     f.write(text)
 
-def create_embeddings():
-    """
-    Get the embeddings for the extracted text
-    :return: None
-    """
-    create_xlm_roberta_embeddings()
 
 def translate_text(files, path, chunk_size=5000, dest_language='en'):
-
     translator = Translator()
     print('-' * 50, "translate_text", '-' * 50)
     for file in files:
@@ -95,7 +88,7 @@ def translate_text(files, path, chunk_size=5000, dest_language='en'):
 
             print(path + "/" + file)
 
-            chunks = [text[i:i+chunk_size] for i in range(0, len(text), chunk_size)]
+            chunks = [text[i:i + chunk_size] for i in range(0, len(text), chunk_size)]
             translated_chunks = []
             for chunk in chunks:
                 translated = translator.translate(chunk, dest=dest_language)
@@ -104,10 +97,11 @@ def translate_text(files, path, chunk_size=5000, dest_language='en'):
             with open('translated_text/' + path + "/" + file[:-4] + '.txt', 'w+', encoding="utf-8") as f:
                 f.write(translated_text)
 
+
 def create_translated_text():
-    benign_mislead_path = sys.argv[1] # extracted_benign_misleading_text
+    benign_mislead_path = sys.argv[1]  # extracted_benign_misleading_text
     benign_mislead_files = os.listdir(benign_mislead_path)
-    phishing_path = sys.argv[2] # extracted_phishing_text
+    phishing_path = sys.argv[2]  # extracted_phishing_text
     phishing_files = os.listdir(phishing_path)
     os.makedirs('translated_text/' + benign_mislead_path, exist_ok=True)
     os.makedirs('translated_text/' + phishing_path, exist_ok=True)
@@ -117,8 +111,8 @@ def create_translated_text():
 
 
 def generate_sbert_embeddings_arrays(files, path):
-
-    model = SentenceTransformer('sentence-transformers/bert-base-nli-mean-tokens')
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = SentenceTransformer('sentence-transformers/bert-base-nli-mean-tokens').to(device)
     sentence_bert_embeddings = np.array([]).reshape(0, 768)
     sentence_bert_embeddings_labels = np.array([]).reshape(0, 1)
     print("-" * 50, "Sentence BERT", "-" * 50)
@@ -126,20 +120,21 @@ def generate_sbert_embeddings_arrays(files, path):
         with open('translated_text/' + path + "/" + file, 'r', encoding="utf-8") as f:
             text = f.read()
         print(path + "/" + file)
-        sentence_embeddings = model.encode(text)
+        sentence_embeddings = model.encode(text).to(device)
         sentence_embeddings = sentence_embeddings.reshape(1, -1)
         sentence_bert_embeddings = np.concatenate((sentence_bert_embeddings, sentence_embeddings))
 
-        if path == sys.argv[1]:
+        if path == sys.argv[3]:
             sentence_bert_embeddings_labels = np.concatenate(
                 (sentence_bert_embeddings_labels, np.zeros((len(sentence_embeddings), 1))))
 
-        else:
+        elif path == sys.argv[4]:
             sentence_bert_embeddings_labels = np.concatenate(
                 (sentence_bert_embeddings_labels, np.ones((len(sentence_embeddings), 1))))
 
     result = np.concatenate((sentence_bert_embeddings, sentence_bert_embeddings_labels), axis=1)
     return result, sentence_bert_embeddings_labels
+
 
 def generate_xlm_roberta_embeddings_arrays(files, path):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -165,18 +160,18 @@ def generate_xlm_roberta_embeddings_arrays(files, path):
         with torch.no_grad():
             model_output = model(**encoded_input.to(device))
             sentence_embeddings = mean_pooling(model_output, encoded_input['attention_mask']).to(device)
-            xlm_roberta_embeddings = np.concatenate((xlm_roberta_embeddings, sentence_embeddings.cpu().numpy()))
-            if path == sys.argv[1]:
+            xlm_roberta_embeddings = np.concatenate((xlm_roberta_embeddings, sentence_embeddings.to('cpu').numpy()))
+            if path == sys.argv[3]:
                 xlm_roberta_embeddings_labels = np.concatenate(
                     (xlm_roberta_embeddings_labels, np.zeros((len(sentence_embeddings), 1))))
 
-            else:
+            elif path == sys.argv[4]:
                 xlm_roberta_embeddings_labels = np.concatenate(
                     (xlm_roberta_embeddings_labels, np.ones((len(sentence_embeddings), 1))))
 
-
     result = np.concatenate((xlm_roberta_embeddings, xlm_roberta_embeddings_labels), axis=1)
     return result, xlm_roberta_embeddings_labels
+
 
 def create_transformers_embeddings(transformers_name):
     benign_mislead_path = sys.argv[3]
@@ -184,12 +179,12 @@ def create_transformers_embeddings(transformers_name):
     phishing_path = sys.argv[4]
     phishing_files = os.listdir(phishing_path)
     os.makedirs('embeddings', exist_ok=True)
-    
+
     if transformers_name == "xlm-roberta":
-        result_legitimate, label_leg = generate_xlm_embeddings_arrays(benign_mislead_files, benign_mislead_path)
+        result_legitimate, label_leg = generate_xlm_roberta_embeddings_arrays(benign_mislead_files, benign_mislead_path)
         # print("result_legitimate shape", result_legitimate.shape)
 
-        result_phishing, label_phis = generate_xlm_embeddings_arrays(phishing_files, phishing_path)
+        result_phishing, label_phis = generate_xlm_roberta_embeddings_arrays(phishing_files, phishing_path)
         # print("result_phishing shape", result_phishing.shape)
 
         result = np.concatenate((result_legitimate, result_phishing), axis=0)
@@ -208,43 +203,13 @@ def create_transformers_embeddings(transformers_name):
         # print("result labels", result[:-1, :])
         with open('embeddings/' + 'sbert.pkl', 'wb') as file:
             pickle.dump(result, file)
-            
-def create_xlm_roberta_embeddings():
-    """
-    Get the XLM-Roberta embeddings for the extracted text
-    :return: None
-    """
-    # Get the extracted text from the benign and phishing folders
-    benign_mislead_path = sys.argv[3]
-    benign_mislead_files = os.listdir(benign_mislead_path)
-    phishing_path = sys.argv[4]
-    phishing_files = os.listdir(phishing_path)
-    os.makedirs('embeddings', exist_ok=True)
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-
-    result_legitimate, label_leg = generate_xlm_roberta_embeddings_arrays(
-        benign_mislead_files, benign_mislead_path)
-    print("result_legitimate shape", result_legitimate.shape)
-    # print("label_leg shape", label_leg.shape)
-    # print("label_leg", label_leg)
-    result_phishing, label_phis = generate_xlm_roberta_embeddings_arrays(
-        phishing_files, phishing_path)
-    print("result_phishing shape", result_phishing.shape)
-    # print("label_phis shape", label_phis.shape)
-    # print("label_phis", label_phis)
-    result = np.concatenate((result_legitimate, result_phishing), axis=0)
-
-    print("result shape", result.shape)
-    print("result", result[:-1, :])
-    with open('embeddings/' + 'legitimate_embeddings-xlm-roberta.pkl', 'wb') as file:
-        pickle.dump(result, file)
 
 
 def mean_pooling(model_output, attention_mask):
-    token_embeddings = model_output[0] #First element of model_output contains all token embeddings
+    token_embeddings = model_output[0]  # First element of model_output contains all token embeddings
     input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
     return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+
 
 if __name__ == '__main__':
     # Parameters:
@@ -253,30 +218,8 @@ if __name__ == '__main__':
     # 3. Path to the extracted benign and misleading text files
     # 4. Path to the extracted phishing text files
     # extract_text()
-    # create_embeddings()
-    create_translated_text()
+    # create_translated_text()
     create_transformers_embeddings("xlm-roberta")
-    create_transformers_embeddings("sbert")
+    # create_transformers_embeddings("sbert")
     # XLM Roberta Results:
     # result shape(49492, 769)
-    # result[[-0.01027331 - 0.00268482 - 0.01249031...  0.07113912  0.10873187
-    #         1.]
-    # [0.01801311
-    # 0.00493359
-    # 0.02154984...
-    # 0.04630521
-    # 0.16071656
-    # 1.]
-    # [0.0526188   0.03298641  0.00592623...  0.03206075  0.10819747
-    #  1.]
-    # ...
-    # [-0.00757216
-    # 0.01041496 - 0.01022783... - 0.02627281
-    # 0.04426541
-    # 1.]
-    # [-0.00467764  0.00345328 - 0.00849169... - 0.02927882  0.05683431
-    #  1.]
-    # [-0.00757216
-    # 0.01041496 - 0.01022783... - 0.02627281
-    # 0.04426541
-    # 1.]]
